c19031120e24e66207cf4275686f3c642e508e19
Lawrence Mungai <mungailawrence52@gmail.com>
2026-09-13 22:45:09 +0300

    Fix the whatsappLink

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,5 +1,5 @@
 export const WHATSAPP_URL =
-  "https://wa.me/245780172069?text=Hello%2C%20can%20I%20get%20more%20info%20on%20your%20services%3F";
+  "https://wa.me/254780172069?text=Hello%2C%20can%20I%20get%20more%20info%20on%20your%20services%3F";
 
 export const PHONE_PRIMARY = "+254 780 172 069";
 export const PHONE_SECONDARY = "+254 747 641 465";
